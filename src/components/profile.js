38d3dfa1e5dde021939eab8d3ad57e8c17227b4d
snow <liuxueworking@gmail.com>
2021-07-30 20:03:29 -0700

    add search tag bar

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,7 +3,9 @@ import Test from './test'
 
 const Profile =(props)=>{
     const {item} = props
+    const [student,setStudent]=useState(item)
     const [display,setDisplay] = useState(true)
+    const [tag,setTag] = useState([])
 
     const handleToggle = (e) =>{
         e.preventDefault();
@@ -11,6 +13,21 @@ const Profile =(props)=>{
         return display ? document.getElementsByClassName('tests')[e.target.id-1].style.display = 'block'
                        : document.getElementsByClassName('tests')[e.target.id-1].style.display = 'none'
     }
+
+    const addTag = (e) =>{
+        const {name,value} = e.target
+        e.preventDefault();
+        if(e.key==='Enter'){
+            setTag([...tag,value]);
+            setStudent((preStudent)=>({
+                ...preStudent,
+                [name]:[value]
+            }))
+            return e.target.value = ''; 
+        }
+    }
+
+    console.log(student);
 
     return (
         <div>
@@ -20,13 +37,19 @@ const Profile =(props)=>{
                     <img className='photo col-3' src={item.pic} alt={item.id}/>
                     <div className='col-8'>
                         <p className='name'>{item.firstName} {item.lastName}</p>
-                        <div className='info'>
-                            <p className='text'>Email: {item.email} </p>
-                            <p className='text'>Company: {item.company} </p>
-                            <p className='text'>Skill: {item.skill} </p>
-                            <p className='text'>Average: {item.grades.reduce((a,b)=>parseFloat(a)+parseFloat(b),0) / item.grades.length}% </p>
+                        <div className='indent'>
+                            <div className='info'>
+                                <p className='text'>Email: {item.email} </p>
+                                <p className='text'>Company: {item.company} </p>
+                                <p className='text'>Skill: {item.skill} </p>
+                                <p className='text'>Average: {item.grades.reduce((a,b)=>parseFloat(a)+parseFloat(b),0) / item.grades.length}% </p>
+                            </div>
+                            <Test item ={item}/>
+                            <div>
+                                {tag.map((item,index)=><label key={`${index}-{item.id}`}>{item}</label>)}
+                            </div>
+                            <input placeholder='Add a tag' className='tag' onKeyUp ={addTag} name='tag'></input>
                         </div>
-                        <Test item ={item}/>
                     </div>
                     <a href='/' className='col-1' onClick={(e)=>handleToggle(e)}>
                         {
--- src/components/profiles.js
+++ src/components/profiles.js
@@ -8,7 +8,8 @@ import Profile from './profile'
 // to fetch Object data, and present the information on the screen.
 const Profiles =()=>{
     const [data,setData]=useState([])
-    const [input, setInput] = useState('')
+    const [inputName, setInputName] = useState('')
+    const [inputTag, setInputTag] = useState('')
 
     useEffect(()=>{
         fetchData(DATA.URL)
@@ -17,15 +18,27 @@ const Profiles =()=>{
         })
     },[]);
 
+    const searchTagHandler = (e) =>{
+        setInputTag(e.target.value)
+    }
+
     return(
         <div>
-            <input placeholder='Search by Name' id='input' onChange={(e)=>setInput(e.target.value)}></input>
+            <div className='searchSection'>
+                <input placeholder='Search by Name' id='searchByName' onChange={(e)=>setInputName(e.target.value)}></input>
+                <input placeholder='Search by Tag' id='searchByTag' onChange={searchTagHandler}></input>
+            </div>
             <div className='profiles'>
             {
                 data.filter((item)=>
-                    input==='' ? item
-                               : item.firstName.toLowerCase().includes(input.toLowerCase()) || item.lastName.toLowerCase().includes(input.toLowerCase())
-                                    ? item : ''
+                    inputName==='' ? item
+                                   : item.firstName.toLowerCase().includes(inputName.toLowerCase()) || item.lastName.toLowerCase().includes(inputName.toLowerCase())
+                                        ? item : ''
+                )
+                .filter((item)=>
+                    inputTag ==='' ? item
+                                : item.tag.toLowerCase().includes(inputTag.toLowerCase()) || item.tag.toLowerCase().includes(inputTag.toLowerCase())
+                                        ? item : ''
                 )
                 .map((item,key)=> <Profile item ={item} key={key}/>)
             }
